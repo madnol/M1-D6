735b61ca6d95c9862b532c6b595350caaeb5863d
Manuel Desole <46155957+madnol@users.noreply.github.com>
2020-10-05 16:54:36 +0200

    M2-D6 FIRST

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector('#header');
+let alignElement = document.querySelector('#header, h1');
+alignElement.style.textAlign = "center";
+
+document.querySelector('body');
+let bgColor = document.querySelector('body');
+bgColor.style.backgroundColor = "#f7f7f7";
